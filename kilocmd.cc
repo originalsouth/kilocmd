15047d8b1c136a5d9f6947d3104b24982542da26
zuiden <zuiden@ilorentz.org>
2014-09-03 12:32:31 +0200

    Implemented ability to upload and some more

--- kilocmd.cc
+++ kilocmd.cc
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ftdi.h>
+#include <unistd.h>
 #include "intelhex.h"
 
 struct ftdi_context *ftdic;
@@ -107,14 +108,85 @@ void send_cmd(char *cmd)
     {
         if(ftdi_write_data(ftdic,(unsigned char *)cmd,132)==132)
         {
-            if(!silent) puts("kilocmd: succesfully sent command!");
+            if(!silent) puts("kilocmd: successfully sent command!");
         }
         else if(!silent) puts("kilocmd: error sending command");
     }
     else if(!silent) puts("kilocmd: not connected to device...");
 }
 
-void send_program(char *fname); //TODO
+void send_program(const char *fname)
+{
+    if(ftdic)
+    {
+        unsigned char prgm[132];
+        FILE *file=fopen(fname,"r");
+        if(!file)
+        {
+            if(!silent) printf("kilocmd: unable to open file \"%s\"\n",fname);
+            return;
+        }
+        fclose(file);
+        data.load(fname);
+        uint8_t pg_t=1+data.size()/128;
+        if(pg_t>220) pg_t=220;
+        uint8_t pg=pg_t;
+        for(unsigned int i=1;i<3U;)
+        {
+            if(pg>=pg_t)
+            {
+                pg=0;
+                memset(prgm,0,132);
+                prgm[0]=0x55,prgm[1]=0x2,prgm[2]=pg_t,prgm[11]=0x82,prgm[131]=0x55^0x2^pg_t^0x82;
+                if(ftdi_write_data(ftdic,prgm,132)!=132)
+                {
+                    if(!silent) puts("kilocmd: error uploading program");
+                    return;
+                }
+                i++;
+            }
+            else
+            {
+                prgm[0]=0x55,prgm[1]=0x4,prgm[2]=pg;
+                uint8_t cksm=0x55^0x4^pg,dbyte;
+                for(unsigned int j=0;j<128;j++)
+                {
+                    dbyte=data.get(pg*128+j);
+                    prgm[j+3]=dbyte;
+                    cksm^=dbyte;
+                }
+                prgm[131]=cksm;
+                if(ftdi_write_data(ftdic,prgm,132)!=132)
+                {
+                    if(!silent) puts("kilocmd: error uploading program");
+                    return;
+                }
+                else pg++;
+            }
+            usleep(130000);
+        }
+    }
+    else if(!silent) puts("kilocmd: not connected to device...");
+    if(!silent) printf("kilocmd: successfully uploaded \"%s\"!",fname);
+}
+
+char* strtokar(char *source,char delim,unsigned int pos)
+{
+    static char dest[4096];
+    unsigned int i=0,dlim=0;
+    for(;dlim<pos;i++)
+    {
+        if(source[i]==delim) dlim++;
+        else if(source[i]==0)
+        {
+            dest[0]=0;
+            return dest;
+        }
+    }
+    strcpy(dest,&source[i]);
+    for(i=0;dest[i]>0;i++) if(dest[i]==delim) dest[i]=0;
+    return dest;
+}
 
 void proccess(FILE *file)
 {
@@ -127,17 +199,22 @@ void proccess(FILE *file)
         fgets(stroutel,4096,file);
         for(size_t i=0;i<strlen(stroutel);i++) if(stroutel[i]=='\n') stroutel[i]=0;
         if(!strcmp(stroutel,"\0")) continue;
-        else if(!strcmp(stroutel,"VOLTAGE")) send_cmd(VOLTAGE);
-        else if(!strcmp(stroutel,"CHARGE")) send_cmd(CHARGE);
-        else if(!strcmp(stroutel,"PAUSE")) send_cmd(PAUSE);
-        else if(!strcmp(stroutel,"RUN")) send_cmd(RUN);
-        else if(!strcmp(stroutel,"RESET")) send_cmd(RESET);
-        else if(!strcmp(stroutel,"SLEEP")) send_cmd(SLEEP);
-        else if(!strcmp(stroutel,"TOGGLE")) send_cmd(LEDTOGGLE);
-        else if(!strcmp(stroutel,"STOP")) send_cmd(STOP);
-        else if(!strcmp(stroutel,"BOOTLOAD")) send_cmd(BOOTLOAD);
-        else if(!strcmp(stroutel,"UPLOAD")) puts("kilocmd: to be implemented");
-        else fprintf(stderr,"kilocmd: error parsing command %s\n",stroutel);
+        else if(!strcmp(strtokar(stroutel,' ',0),"VOLTAGE")) send_cmd(VOLTAGE);
+        else if(!strcmp(strtokar(stroutel,' ',0),"CHARGE")) send_cmd(CHARGE);
+        else if(!strcmp(strtokar(stroutel,' ',0),"PAUSE")) send_cmd(PAUSE);
+        else if(!strcmp(strtokar(stroutel,' ',0),"RUN")) send_cmd(RUN);
+        else if(!strcmp(strtokar(stroutel,' ',0),"RESET")) send_cmd(RESET);
+        else if(!strcmp(strtokar(stroutel,' ',0),"SLEEP")) send_cmd(SLEEP);
+        else if(!strcmp(strtokar(stroutel,' ',0),"TOGGLE")) send_cmd(LEDTOGGLE);
+        else if(!strcmp(strtokar(stroutel,' ',0),"STOP")) send_cmd(STOP);
+        else if(!strcmp(strtokar(stroutel,' ',0),"BOOTLOAD")) send_cmd(BOOTLOAD);
+        else if(!strcmp(strtokar(stroutel,' ',0),"UPLOAD")) send_program(strtokar(stroutel,' ',1));
+        else if(!strcmp(strtokar(stroutel,' ',0),"STFU"))
+        {
+            silent=not silent;
+            if(!silent) puts("kilcmd: are we talking again?");
+        }
+        else fprintf(stderr,"kilocmd: error parsing command \"%s\"\n",stroutel);
     }
     close();
 }
@@ -149,19 +226,20 @@ void help()
     puts("usage: kilocmd [options] \"filename\"");
     puts("if no filename is provided stdin is used (use ^D to exit)");
     puts("options:");
-    puts("\t-s\t\t\tstfu or silent mode");
+    puts("\t-s or --stfu\tstartup in silent mode");
     puts("\t-h or --help\tproduce this output");
     puts("commands:");
-    puts("\tVOLTAGE");
-    puts("\tCHARGE");
-    puts("\tPAUSE");
-    puts("\tRUN");
-    puts("\tRESET");
-    puts("\tSLEEP");
-    puts("\tTOGGLE");
-    puts("\tSTOP");
-    puts("\tBOOTLOAD");
-    puts("\tUPLOAD filename (to be implemented)"); //TODO
+    puts("\tVOLTAGE\t\tdisplay voltage level using LED (blue/green = fully charged, yellow/red = need battery)");
+    puts("\tCHARGE\t\tsets the kilobot in charge mode");
+    puts("\tPAUSE\t\tpause the user program");
+    puts("\tRUN\t\truns the currently uploaded program");
+    puts("\tRESET\t\tjump to the user program starting point");
+    puts("\tSLEEP\t\tswitch to low-power sleep mode");
+    puts("\tTOGGLE\t\ttoggle LEDs on controller");
+    puts("\tSTOP\t\tsends an empty message");
+    puts("\tBOOTLOAD\tjump to their bootloader to accept a new program");
+    puts("\tUPLOAD filename\tsend a new program to kilobots that are in bootloader mode");
+    puts("\tSTFU\t\ttoggle silent mode");
 }
 
 int main(int argc,char *argv[])
@@ -170,6 +248,7 @@ int main(int argc,char *argv[])
     else if(argc==2)
     {
         if(argv[1][0]=='-' and argv[1][1]=='s') silent=true;
+        else if(argv[1][0]=='-' and argv[1][1]=='-' and !strcmp(&argv[1][2],"stfu")) silent=true;
         else if(argv[1][0]=='-' and argv[1][1]=='h')
         {
             help();
